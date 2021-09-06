5c76ee7c307070bd95107334f2d51ee9302a1e58
Santiago De Olivera <santiago.deolivera@correo.ucu.edu.uy>
2021-09-06 10:53:35 -0300

    Exercise resolution

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PII_Game_Of_Life
 {
@@ -6,7 +7,17 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = @"../../assets/board.txt";
+            string content = File.ReadAllText(url);
+            GameOfLife game = new GameOfLife(content);
+            Console.WriteLine(game.ToText());
+            while(true)
+            {
+                Console.ReadKey();
+                for(byte i = 0; i < 8; i++) Console.WriteLine("");
+                game.Update();
+                Console.WriteLine(game.ToText());
+            }
         }
     }
 }
